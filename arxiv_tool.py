42104bb4963685362a96144a3e0d87893d2fe682
Riddhish Bonde <bonderiddhish@gmail.com>
2025-08-30 00:02:29 +0530

    parsed arxiv

--- arxiv_tool.py
+++ arxiv_tool.py
@@ -1,5 +1,6 @@
 #Access arxiv using url
 import requests
+import xml.etree.ElementTree as ET
 
 def search_arxiv_paper(topic: str,max_results: int=5) -> dict:
     query = "+".join(topic.lower().split())
@@ -24,4 +25,36 @@ def search_arxiv_paper(topic: str,max_results: int=5) -> dict:
     return data
 
 
+#parsing xml
+def parse_arxiv_xml(xml_content: str) -> dict:
+    entries = []
+    ns = {
+        "atom": "http://www.w3.org/2005/Atom",
+        "arxiv": "http://arxiv.org/schemas/atom"
+    }
+    root = ET.fromstring(xml_content)
+    for entry in root.findall("atom:entry",ns):
+        authors = [
+            author.findtext("atom:name",namespace=ns)
+            for author in entry.findall("atom:author",ns)
+        ]
+
+        categories = [
+            cat.attrib.get("term")
+            for cat in entry.finall("atom:category",ns)
+        ]
+
+        pdf_link = None
+        for link in entry.findall("atom:link",ns):
+            if link.attrib.get("type") == "application/pdf":
+                pdf_link = link.attrib.get("href")
+                break
+        entries.append({
+            "title": entry.findtext("atom:title",namespaces=ns),
+            "summary":entry.findtext("atom:summary",namespaces=ns).strip(),
+            "authors":authors,
+            "categories": categories,
+            "pdf": pdf_link
+        })
+    return {"entries": entries}
 
